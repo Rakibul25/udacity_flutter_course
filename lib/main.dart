52500744ed5b0670097fd418c0ceb6325df0cdc8
Md Rakibul Islam <38698778+Rakibul25@users.noreply.github.com>
2021-01-31 16:14:52 +0600

    Final

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,36 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyHomePage(),
-    );
-  }
+void main(){
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Hello Rectangle',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello Rectangle'),
+        ),
+        body: HelloRectangle(),
+      ),
+    ),
+  );
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage();
-  @override
-  createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
+class HelloRectangle extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-            ),
-          ],
+    // TODO: implement build
+    return Center(
+      child: Container(
+        color: Colors.greenAccent,
+        height: 400.0,
+        width: 300.0,
+        child: Center(
+          child: Text('Hello',
+            style: TextStyle(fontSize: 40.0),
+            textAlign: TextAlign.center,),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () { _counter++; },
-        child: Icon(Icons.add),
-      ),
     );
   }
+
 }
